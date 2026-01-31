7e32af99fac68ed695f12df758689c5ead7e3df3
Madhusudhan-2 <madhusudhansrvm2421@gmail.com>
2026-01-31 14:27:17 +0530

    Update And_Gate.sv

--- And_Gate.sv
+++ And_Gate.sv
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------------
 //File       : AND_GATE.sv
-//Author     : Krishna Gupta/1BM23EC123
-//Created    : 2026-01-28
+//Author     : Madhusudhan C/1BM23EC134
+//Created    : 2026-01-29
 //Module     : AND_GATE
 //Project    : SystemVerilog and Verification (23EC6PE2SV),
 //Faculty    : Prof. Ajaykumar Devarapalli
